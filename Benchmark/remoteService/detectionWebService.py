e8ff4b11fe09d77b7f4595316b5d1a5f1dadf6d0
Tomá Zikmund <tommekster@gmail.com>
2018-08-31 18:23:46 +0200

    #2536 - oprava typu komunit

--- Benchmark/remoteService/detectionWebService.py
+++ Benchmark/remoteService/detectionWebService.py
@@ -22,23 +22,27 @@ class DetectionWebService(object):
     def bigClam(self, graph : nx.Graph, comsNum=-1):
         if not comsNum: comsNum = -1
         edges = self._getEdges(graph)
-        return self.proxy.bigClam(edges, comsNum)
+        partitions = self.proxy.bigClam(edges, comsNum)
+        return self._bipartitePartitions(graph, partitions)
     
     def louvain(self, graph : nx.Graph):
         edges = self._getEdges(graph)
         nodes = self._getNodes(graph)
-        return self.proxy.louvain(nodes, edges)
+        partitions = self.proxy.louvain(nodes, edges)
+        return self._bipartitePartitions(graph, partitions)
     
     def olapSBM(self, graph : nx.Graph, maxComs=10, runsPerNetwork=10):
         if not maxComs: maxComs = 10
         edges = self._getEdges(graph)
-        return self.proxy.olapSBM(edges, maxComs, runsPerNetwork)
+        multiplePartitions = self.proxy.olapSBM(edges, maxComs, runsPerNetwork)
+        return tuple([self._bipartitePartitions(graph, partitions) for partitions in multiplePartitions])
     
     def biSBM(self, graph : nx.Graph, Ka=5, Kb=5, isDegreeCorrected=True, KLsteps=5):
         if not Ka or not Kb: Ka, Kb = 5, 5
         edges = self._getEdges(graph)
         types = self._getTypes(graph)
-        return self.proxy.biSBM(edges, types, Ka, Kb, isDegreeCorrected, KLsteps)
+        partitions = self.proxy.biSBM(edges, types, Ka, Kb, isDegreeCorrected, KLsteps)
+        return self._bipartitePartitions(graph, partitions)
     
     def _getEdges(self, graph : nx.Graph):
         return list(graph.edges)
@@ -47,9 +51,19 @@ class DetectionWebService(object):
         return list(graph.nodes)
     
     def _getTypes(self, graph: nx.Graph):
-        return list(self.__generateTypes(graph))
+        types = list(self.__generateTypes(graph))
+        types.sort(key=lambda T: T[0])
+        return [T[1] for T in types]
     
     def __generateTypes(self, graph):
         for n in graph.nodes:
             node = graph.node[n]
-            yield node['type'] + 1 if 'type' in node else 1
+            yield [int(n), node['type'] + 1 if 'type' in node else 1]
+            
+    def _bipartitePartitions(self, graph:nx.Graph, partitions):
+        typeSets = self.__nodeTypeSets(graph)
+        return [list(typeSet & set(P)) for P in partitions for typeSet in typeSets]
+    
+    def __nodeTypeSets(self, graph):
+        types = self._getTypes(graph)
+        return [set([n + 1 for n, t in enumerate(types) if t == T]) for T in (1, 2)]
